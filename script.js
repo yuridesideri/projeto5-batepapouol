9ad8fd2099f4de00bb948c0222efd456ca1dcba7
Yuri <yuridesideri12@gmail.com>
2022-08-23 18:52:58 -0300

    login e logout without backend implemented - also in and out animations

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("Hello World!")
 
+//User Properties
 const user = {
     connection: "Not connected",
     userName: "",
@@ -14,6 +15,10 @@ function logIn(username)
     {
         user.connection = "Connected";
         user.userName = username;
+        document.querySelector(".entry-page").classList.add("logIn-screen-animation");
+        setTimeout(()=>{document.querySelector(".entry-page").classList.add("hide-element");}, 1000);
+        setTimeout(()=>{document.querySelector(".entry-page").classList.remove("logIn-screen-animation");}, 1000);
+        document.querySelector(".main-chat").classList.toggle("hide-element");
     }
     else{
         let warning = document.createElement("p")
@@ -23,4 +28,27 @@ function logIn(username)
         parent.appendChild(warning);
         setTimeout(() => {parent.removeChild(warning)}, 1100);
     }
+}
+
+
+//SideBar Functions
+function sideBarComeUp(element)
+{
+    let sidebar = document.querySelector("nav");
+    element.classList.add("block-click");
+    setTimeout(()=>{element.classList.remove("block-click");}, 300);
+    sidebar.classList.remove("hide-element");
+    sidebar.classList.add("block-click");
+    sidebar.children[1].classList.add("side-bar-animation-comeIn");
+    setTimeout(()=>{sidebar.children[1].classList.remove("side-bar-animation-comeIn");}, 790);
+    setTimeout(()=>{sidebar.classList.remove("block-click");}, 790);
+}
+
+function sideBarReturn()
+{
+    let sidebar = document.querySelector("nav");
+    sidebar.children[1].classList.add("side-bar-animation-comeOut");
+    setTimeout(()=>{sidebar.children[1].classList.remove("side-bar-animation-comeOut");}, 800);
+    setTimeout(()=>{sidebar.classList.add("hide-element");}, 780);
+
 }
