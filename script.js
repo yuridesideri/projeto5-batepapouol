decb6cbab4d7354134cf7c4477088d2e44fcf8a2
Yuri <yuridesideri12@gmail.com>
2022-08-23 21:25:32 -0300

    Login with backend implemented

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-console.log("Hello World!")
-
 //User Properties
 const user = {
     connection: "Not connected",
     userName: "",
-    connectionTime: ""
+    connectionTime: "",
+    statusUpdate: ""
 }
 
 //Run-Time Functions:
-function logIn(username)
+function logIn(username, check)
 {
-    console.log(username); //Remover Depois
-    if (username.length <= 20 && username !== "" && !(username.includes(" ")))
+    check = check || 0;
+    let inputBox = document.querySelector(".entry-page input");
+    let loadGif = document.querySelector(".entry-page>img");
+    if (check === 400) //Falhou no LogIn - nome em uso
     {
+        inputBox.classList.toggle('hide-element');
+        loadGif.classList.toggle('hide-element');
+        let warning = document.createElement("p")
+        warning.innerHTML = "Este nome está em uso!"
+        warning.classList.add("logIn-warning");
+        let parent = document.querySelector(".entry-page")
+        parent.appendChild(warning);
+        setTimeout(() => {parent.removeChild(warning)}, 1100);
+    }
+    else if (check === 200) //Sucesso no LogIn
+    {
+        inputBox.classList.toggle('hide-element');
+        loadGif.classList.toggle('hide-element');
         user.connection = "Connected";
         user.userName = username;
+        user.statusUpdate = setInterval(()=>{axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: username})}, 5000);
         document.querySelector(".entry-page").classList.add("logIn-screen-animation");
         setTimeout(()=>{document.querySelector(".entry-page").classList.add("hide-element");}, 1000);
         setTimeout(()=>{document.querySelector(".entry-page").classList.remove("logIn-screen-animation");}, 1000);
         document.querySelector(".main-chat").classList.toggle("hide-element");
     }
-    else{
+
+    else if (check === 0 & username.length <= 20 && username !== "" && !(username.includes(" "))) //Bom nome, checando se está em uso.
+    {
+        inputBox.classList.toggle('hide-element');
+        loadGif.classList.toggle('hide-element');
+        let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: username});
+        promise.then((response)=>{logIn(username, response.status)});
+        promise.catch((response)=>{logIn(username, response.response.status)});
+    }   
+    else //Falha no LogIn nome com caracteres ruins
+    { 
         let warning = document.createElement("p")
         warning.innerHTML = "Digite um username válido!"
         warning.classList.add("logIn-warning");
